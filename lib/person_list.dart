8dc37e2f6d267391b1282cb33837f34b2f028890
Наиль Шарипов <cripnail@gmail.com>
2022-09-30 18:19:52 +0500

    Implement popover for web/desktop

--- lib/person_list.dart
+++ lib/person_list.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
 import 'package:responsive_adaptive/persons.dart';
 
 class PersonList extends StatelessWidget {
@@ -10,56 +12,127 @@ class PersonList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: persons.length,
-        itemBuilder: (BuildContext context, int index) {
-          final person = persons[index];
-          return Container(
-            color:
-                index == currentPerson ? Colors.lightBlueAccent : Colors.white,
-            child: ListTile(
-              leading: FittedBox(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(person.picture),
-                  radius: 30, // no matter how big it is, it won't overflow
-                ),
-              ),
-              title: Text(person.title),
-              subtitle: Text(person.email),
-              trailing: Text(person.grade.toString()),
-              onTap: () {
-                onPersonTap(index);
-                showModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Container(
-                        height: 200,
-                        color: Colors.white,
-                        child: Column(children: const <Widget>[
-                          ListTile(
-                            leading: Icon(Icons.person),
-                            title: Text('VIEW PROFILE'),
-                          ),
-                          ListTile(
-                            leading: Icon(Icons.people),
-                            title: Text('FRIENDS'),
-                          ),
-                          ListTile(
-                            leading: Icon(Icons.report),
-                            title: Text('REPORT'),
-                          ),
-                        ]),
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      double width = constraints.constrainWidth();
+      if (kDebugMode) {
+        print(width);
+      }
+
+      return width < 720
+          ? ListView.builder(
+              itemCount: persons.length,
+              itemBuilder: (BuildContext context, int index) {
+                final person = persons[index];
+                return Container(
+                  color: index == currentPerson
+                      ? Colors.lightBlueAccent
+                      : Colors.white,
+                  child: ListTile(
+                    leading: FittedBox(
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage(person.picture),
+                        radius:
+                            30, // no matter how big it is, it won't overflow
                       ),
-                    );
-                  },
+                    ),
+                    title: Text(person.title),
+                    subtitle: Text(person.email),
+                    trailing: Text(person.grade.toString()),
+                    onTap: () {
+                      onPersonTap(index);
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Container(
+                              height: 200,
+                              color: Colors.white,
+                              child: Column(children: const <Widget>[
+                                ListTile(
+                                  leading: Icon(Icons.person),
+                                  title: Text('VIEW PROFILE'),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.people),
+                                  title: Text('FRIENDS'),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.report),
+                                  title: Text('REPORT'),
+                                ),
+                              ]),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
                 );
-              },
-            ),
-          );
-        });
+              })
+          : GridView.builder(
+              itemCount: persons.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                final person = persons[index];
+                return Container(
+                  color: index == currentPerson
+                      ? Colors.lightBlueAccent
+                      : Colors.white,
+                  child: ListTile(
+                    title: CircleAvatar(
+                      backgroundImage: AssetImage(person.picture),
+                      radius:
+                      110, // no matter how big it is, it won't overflow
+                    ),
+                    subtitle: Column(
+                      children: [
+                        Text(person.title),
+                        Text(person.email),
+                        Text(person.grade.toString()),
+                      ],
+                    ),
+                    onTap: () {
+                      onPersonTap(index);
+                      showPopover(
+                        context: context,
+                        bodyBuilder: (BuildContext context) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Container(
+                              height: 200,
+                              color: Colors.white,
+                              child: Column(children: const <Widget>[
+                                ListTile(
+                                  leading: Icon(Icons.person),
+                                  title: Text('VIEW PROFILE'),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.people),
+                                  title: Text('FRIENDS'),
+                                ),
+                                ListTile(
+                                  leading: Icon(Icons.report),
+                                  title: Text('REPORT'),
+                                ),
+                              ]),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                );
+              });
+    });
   }
 }
